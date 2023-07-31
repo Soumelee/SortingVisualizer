d9ac801678d6d67d5088eec74202e69fb04baf2b
Soumili Mondal <76765912+Soumelee@users.noreply.github.com>
2023-07-31 19:59:26 +0530

    Add files via upload
    Bubble sort, Selection sort done.

---
+++ bubble.js
@@ -0,0 +1,23 @@
+async function bubbleSort(){
+    console.log("Bubble sort");
+    const bar=document.querySelectorAll(".bar");
+    for (let i = 0; i < bar.length; i++) {
+        for (let j = 1; j < bar.length-i; j++) {
+            bar[j].style.background="#001F54";
+            bar[j-1].style.background="#001F54";
+            
+            await wait(200);
+
+            if(parseInt(bar[j-1].style.height)>(parseInt(bar[j].style.height))){
+                //swap
+                let temp=bar[j-1].style.height;
+                bar[j-1].style.height=bar[j].style.height;
+                bar[j].style.height=temp;
+            }
+            bar[j].style.background="black";
+            bar[j-1].style.background="black";
+        }
+        bar[bar.length-1-i].style.background="#08605F";
+        
+    }
+}
