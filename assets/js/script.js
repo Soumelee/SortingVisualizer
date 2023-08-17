25f396b3b2e6e640ec2eafc8651fdec841414008
Soumili Mondal <soumili.mondal65@gmail.com>
2023-08-17 08:24:04 +0530

    User can input custom array size and values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,37 @@
-const n = 10;//current max 40 looks good
-const array = [];
-init();
-function init() {
-    for (let i = 0; i < n; i++) {
-        array[i] = Math.random()*(1.0 - 0.1) + 0.1;
+custom();
+function init(n) {
+    const customArrayInput = document.getElementById("customArray");
+    const customArrayStr = customArrayInput.value.trim();
+    if (customArrayStr === "") {
+        const array = [];
+        for (let i = 0; i < n; i++) {
+            array[i] = Math.random() * (1.0 - 0.1) + 0.1;
+            array[i]= Math.floor(array[i] * 100)
+        }
+        showBars(array);
     }
-    showBars();
+    else {
+        const array = customArrayStr.split(",").map(item => parseInt(item.trim(), 10));
+        showBars(array);
+    }
+
 }
 const time=200;
 function wait() {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-function showBars(move) {
+function showBars(array, move) {
     document.getElementById("container").innerHTML = "";
     for (let i = 0; i < array.length; i++) {
         const bar = document.createElement("div");
-        bar.style.height = Math.floor(array[i] * 100) + "%";
+        // bar.style.height = Math.floor(array[i] * 100) + "%";
+        bar.style.height = array[i] + "%";
         bar.classList.add("bar");
-        
+
         const beforeElement = document.createElement("div"); // Create a real element for ::before content
         beforeElement.classList.add("bar-before");
-        beforeElement.textContent = Math.floor(array[i] * 100);
+        beforeElement.textContent = array[i];
         bar.appendChild(beforeElement);
 
         if (move && move.indices.includes(i)) {
@@ -30,6 +40,10 @@ function showBars(move) {
         document.getElementById("container").appendChild(bar);
     }
 }
-
+function custom() {
+    const lenInput = document.getElementById("len");
+    const numberOfBars = parseInt(lenInput.value, 10);
+    init(numberOfBars);
+}
 
 
