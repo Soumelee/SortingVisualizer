7f3bbcf5c0d287b07efe0f154f340c0bcbdb43e0
Soumili Mondal <soumili.mondal65@gmail.com>
2023-11-01 01:10:59 +0530

    added quick sort pseudocode

--- assets/js/quick.js
+++ assets/js/quick.js
@@ -1,13 +1,40 @@
 //#08605f is green color for final sorted element or bar
 async function quickSort() {
-    const pause=document.querySelector(".pause");
+    const pause = document.querySelector(".pause");
     pause.style.visibility = "visible";
 
     disableButtons();
 
     console.log("Quick sort");
+
+    let info = document.getElementById("info");
+    info.innerHTML = "";
+    let pseudocode = document.createElement('pre');
+    pseudocode.innerHTML = `
+    function quickSort(arr, l, r) {
+        if (l < r) {
+            const pivotIndex = partition(arr, l, r);
+            quickSort(arr, l, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, r);
+        }
+    }
+    function partition(arr, l, r) {
+        const pivot = arr[r]; 
+        let i = l - 1;    
+        for (let j = l; j < r; j++) { 
+            if (arr[j] < pivot) { 
+                i++;
+                swap(arr, i, j);
+            }
+        }    
+        swap(arr, i + 1, pivot);
+        return i + 1;
+    }  
+    `
+    info.appendChild(pseudocode);
+
     const bar = document.querySelectorAll(".bar");
-    const barBefore=document.querySelectorAll(".bar-before");
+    const barBefore = document.querySelectorAll(".bar-before");
     let l = 0, r = bar.length - 1;
     await quickSortfun(bar, barBefore, l, r);
     pause.style.visibility = "hidden";
@@ -23,29 +50,57 @@ async function quickSortfun(bar, barBefore, l, r) {
     }
     else {
         if (l >= 0 && r >= 0 && l < bar.length && r < bar.length) {
-            bar[r].style.background = "#08605f";
+            bar[r].style.background = "#08605f";//green
             bar[l].style.background = "#08605f";
         }
     }
 }
 async function partition(bar, barBefore, l, r) {
+    let pivottag = document.createElement('span');
+    pivottag.setAttribute('class', 'tags');
+    pivottag.innerHTML = "pivot";
+
+    let jtag = document.createElement('span');
+    jtag.setAttribute('class', 'tags');
+    jtag.innerHTML = "j";
+
+    let itag = document.createElement('span');
+    itag.setAttribute('class', 'tags');
+    itag.innerHTML = "i";
+
+    let i1tag = document.createElement('span');
+    i1tag.setAttribute('class', 'tags');
+    i1tag.innerHTML = "i+1";
+
     //pivot is r
+
     let i = l - 1;
+    try {
+        bar[i].appendChild(itag);
+    } catch (error) {
+
+    }
+
+
     bar[r].style.background = "#a62639";//pivot element red 
+    bar[r].appendChild(pivottag);
+
     for (let j = l; j <= r; j++) {
-        bar[j].style.background = "yellow";  
+        bar[j].style.background = "yellow";
+        bar[j].appendChild(jtag);
         await wait();
 
         if (parseInt(barBefore[j].textContent) < parseInt(barBefore[r].textContent)) {
             i++;
+            bar[i].appendChild(itag);
             //swap
             let temp = bar[i].style.height;
-            bar[i].style.height = bar[j].style.height;      
-            bar[j].style.height = temp;     
-            
+            bar[i].style.height = bar[j].style.height;
+            bar[j].style.height = temp;
+
             let tempBefore = barBefore[i].textContent;
-            barBefore[i].textContent = barBefore[j].textContent;      
-            barBefore[j].textContent = tempBefore;     
+            barBefore[i].textContent = barBefore[j].textContent;
+            barBefore[j].textContent = tempBefore;
 
             bar[i].style.background = "orange";//less than pivot orange
             if (i != j) bar[j].style.background = "orange";
@@ -57,15 +112,16 @@ async function partition(bar, barBefore, l, r) {
         }
     }
     i++;
+    bar[i].appendChild(i1tag);
     await wait();
     //swap pivot to correct position
     let temp = bar[i].style.height;
-    bar[i].style.height = bar[r].style.height;      
-    bar[r].style.height = temp;     
-    
+    bar[i].style.height = bar[r].style.height;
+    bar[r].style.height = temp;
+
     let tempBefore = barBefore[i].textContent;
-    barBefore[i].textContent = barBefore[r].textContent;      
-    barBefore[r].textContent = tempBefore;     
+    barBefore[i].textContent = barBefore[r].textContent;
+    barBefore[r].textContent = tempBefore;
 
     bar[r].style.background = "pink";
     bar[i].style.background = "#08605f";
@@ -73,10 +129,14 @@ async function partition(bar, barBefore, l, r) {
     await wait();
 
     for (let k = 0; k < bar.length; k++) {
-        if (!bar[k].style.background === "#08605f"){
+        if (!bar[k].style.background === "#08605f") {
             bar[k].style.background = "gray";
         }
     }
+    pivottag.remove();
+    itag.remove();
+    jtag.remove();
+    i1tag.remove();
     return i;
 
 }
